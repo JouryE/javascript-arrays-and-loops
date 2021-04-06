154d55987685d6d36b19199434ea52adf4565989
JouryE <jourye@gmail.com>
2021-04-06 16:55:54 +0200

    bezig met mijn huiswerk

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -6,6 +6,7 @@
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
 
+
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -17,7 +19,15 @@
 // loop...
 // klaar!
  // ==========================================
+for (let i = 0; i < 5; i++) {
 
+    if ( i < 4 ) {
+        console.log("Loop...");
+    }
+    if ( i === 4) {
+        console.log("klaar!");
+    }
+}
 
 
 // ==========================================
@@ -32,6 +42,7 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
+
 
 
 
